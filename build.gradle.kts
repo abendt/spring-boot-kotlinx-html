f1f76487aecb4970524234cb3ded0f6c11965ae7
Alphonse Bendt <alphonse.bendt@gmail.com>
2018-03-19 21:48:51 +0100

    add gradle config

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+buildscript {
+    repositories {
+        jcenter()
+    }
+}
+
+plugins {
+    application
+    kotlin("jvm") version "1.2.30"
+    id("org.springframework.boot") version "2.0.0.RELEASE"
+    id("io.spring.dependency-management") version "1.0.4.RELEASE"
+}
+
+repositories {
+    jcenter()
+}
+
+application {
+    mainClassName = "de.flqw.Application"
+}
+
+dependencies {
+    compile(kotlin("stdlib-jdk8"))
+    compile(kotlin("reflect"))
+    compile("org.springframework.boot:spring-boot-starter-web")
+    compile("org.jetbrains.kotlinx:kotlinx-html-jvm:0.6.9")
+}
+
+
+
